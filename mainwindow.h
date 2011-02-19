ca03952085da433723d806a8a383f84a33d46103
Arseny Krasutsky <dtiger@mail.ru>
2011-02-19 23:54:41 +0200

    remove obsolete code

--- mainwindow.h
+++ mainwindow.h
@@ -22,7 +22,6 @@ public slots:
     bool importAllTorikumi();
     void parsingTorikumi12(QString content, int basho, int year, int month, int day, int division);
     void parsingTorikumi3456(QString content, int basho, int year, int month, int day, int division);
-    bool convertHoshitori();
     bool torikumi2Banzuke();
     bool findDate(QString content, int *year, int *month);
     bool insertTorikumi(int id, int basho, int year, int month, int day,
